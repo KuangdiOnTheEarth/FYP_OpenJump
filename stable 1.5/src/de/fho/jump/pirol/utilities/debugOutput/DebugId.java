9e00a00f99f523c5eccb7a77a5c984a4eec660d4
Kuangdi <scygh1@nottingham.ac.uk>
2020-10-30 17:40:57 +0800

    Initial commit. Set up the framework.

---
+++ stable 1.5/src/de/fho/jump/pirol/utilities/debugOutput/DebugId.java
@@ -0,0 +1,17 @@
+/*
+ * Created on 09.10.2006 for PIROL
+ * 
+ * SVN header information:
+ *  $Rev: 409 $
+ *  $Author: mentaer $
+ *  $Date: 2006-11-24 02:56:07 +0800 (周五, 24 11月 2006) $
+ *  $Id: DebugId.java 409 2006-11-23 18:56:07Z mentaer $
+ */
+
+package de.fho.jump.pirol.utilities.debugOutput;
+
+public interface DebugId {
+
+	public abstract String getUserName();
+
+}
